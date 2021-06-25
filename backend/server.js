ec51942de97e7844791f88d9c4776f2c7c770b3e
Matheus <matheusrodrigofilho@outlook.com>
2021-06-25 09:45:30 -0300

    Change somethings

--- backend/server.js
+++
@@ -1,6 +0,0 @@
-const app = require('./config/express')();
-const port = app.get('port');
-
-app.listen(port, () => {
-  console.log(`[SERVER] Servidor rodando na porta ${port}`);
-});
---
+++ backend/src/server.ts
@@ -0,0 +1,10 @@
+import 'reflect-metadata';
+import express from 'express';
+
+import './database';
+
+const app = express();
+
+app.listen(8080, () => {
+  console.log('SERVER IS RUNNING');
+});
